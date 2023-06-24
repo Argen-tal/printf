7cf05a085a2b9c12d9d84a4a1d4e72cfe3fef029
Damilola Olaiya <123437493+Hirosus@users.noreply.github.com>
2023-06-24 16:41:28 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int _putchar(char c);
+int _puts(char *str);
+int _printf(const char *format, ...);
+int print_binary(unsigned int num);
+int print_escaped_string(char *str);
+int print_reversed_string(char *str);
+int print_rot13_string(char *str);
+
+int _putchar(char c)
+{
+   return write(1, &c, 1);
+}
+
+int _puts(char *str)
+{
+    int count = 0;
+    while (*str)
+    {
+        count += _putchar(*str);
+        str++;
+    }
+    return count;
+}
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;  /* Number of characters printed */
+    char buffer[BUFFER_SIZE];  /* Local buffer for efficient printing */
+    int i;
+
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            /* Regular character, print it directly */
+            count += _putchar(format[i]);
+        }
+        else
+        {
+            i++;  /* Skip the '%' */
+
+            /* Check the conversion specifier */
+            switch (format[i])
+            {
+                case 'c':
+                    /* Print a character */
+                    count += _putchar(va_arg(args, int));
+                    break;
+                 case 's':
+                    /* Print a string */
+                    count += _puts(va_arg(args, char *));
+                    break;
+
+                case '%':
+                    /* Print a literal '%' */
+                    count += _putchar('%');
+                    break;
+
+                case 'd':
+                case 'i':
+                    /* Print a decimal integer */
+                    count += sprintf(buffer, "%d", va_arg(args, int));
+                    count += _puts(buffer);
+                    break;
+
+                case 'u':
+                    /* Print an unsigned decimal integer */
+                    count += sprintf(buffer, "%u", va_arg(args, unsigned int));
+                    count += _puts(buffer);
+                    break;
+
+                case 'o':
+                    /* Print an octal integer */
+                    count += sprintf(buffer, "%o", va_arg(args, unsigned int));
+                    count += _puts(buffer);
+                    break;
+
+                case 'x':
+                    /* Print a hexadecimal integer (lowercase) */
+                    count += sprintf(buffer, "%x", va_arg(args, unsigned int));
+                    count += _puts(buffer);
+                    break;
+
+                case 'X':
+                    /* Print a hexadecimal integer (uppercase) */
+                    count += sprintf(buffer, "%X", va_arg(args, unsigned int));
+                    count += _puts(buffer);
+                    break;
+
+                case 'b':
+                    /* Print an unsigned integer in binary format */
+                    count += print_binary(va_arg(args, unsigned int));
+                    break;
+
+                case 'S':
+                    /* Print a string with non-printable characters escaped */
+                    count += print_escaped_string(va_arg(args, char *));
+                    break;
+                case 'p':
+                    /* Print a pointer address */
+                    count += sprintf(buffer, "%p", va_arg(args, void *));
+                    count += _puts(buffer);
+                    break;
+
+                case 'r':
+                    /* Print a reversed string */
+                    count += print_reversed_string(va_arg(args, char *));
+                    break;
+
+                case 'R':
+                    /* Print a ROT13 encoded string */
+                    count += print_rot13_string(va_arg(args, char *));
+                    break;
+
+                default:
+                    /* Invalid conversion specifier, ignore */
+                    break;
+            }
+        }
+    }
+
+    va_end(args);
+    return count;
+}
+
+int print_binary(unsigned int num)
+{
+    int count = 0;
+    int i = 0;
+    int j;
+    char buffer[BUFFER_SIZE];
+
+    if (num == 0)
+    {
+        count += _putchar('0');
+    }
+    else
+    {
+        while (num > 0)
+        {
+            buffer[i++] = num % 2 + '0';
+            num /= 2;
+        }
+
+        for (j = i - 1; j >= 0; j--)
+        {
+            count += _putchar(buffer[j]);
+        }
+    }
+
+    return count;
+}
+
+int print_escaped_string(char *str)
+{
+    int count = 0;
+    while (*str)
+    {
+        if (*str < ' ' || *str >= 127)
+        {
+            /* Non-printable character, escape it */
+            count += _putchar('\\');
+            count += _putchar('x');
+            count += _putchar((*str >> 4) + '0');
+            count += _putchar((*str & 0x0F) + '0');
+        }
+        else
+        {
+            /* Printable character, print it directly */
+            count += _putchar(*str);
+        }
+        str++;
+    }
+    return count;
+}
+
+int print_reversed_string(char *str)
+{
+    int count = 0;
+    int length = 0;
+    char *end = str;
+    int i;
+
+    /* Find the length of the string */
+    while (*end)
+    {
+        length++;
+        end++;
+    }
+
+    /* Print the string in reverse order */
+    for (i = length - 1; i >= 0; i--)
+    {
+        count += _putchar(str[i]);
+    }
+
+    return count;
+}
+int print_rot13_string(char *str)
+{
+    int count = 0;
+    while (*str)
+    {
+        if ((*str >= 'A' && *str <= 'M') || (*str >= 'a' && *str <= 'm'))
+        {
+            /* Rotate characters from A to M */
+            count += _putchar(*str + 13);
+        }
+        else if ((*str >= 'N' && *str <= 'Z') || (*str >= 'n' && *str <= 'z'))
+        {
+            /* Rotate characters from N to Z */
+            count += _putchar(*str - 13);
+        }
+        else
+        {
+            /* Non-alphabetic character, print it directly */
+            count += _putchar(*str);
+        }
+        str++;
+    }
+    return count;
+}
