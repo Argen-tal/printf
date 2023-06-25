63748800034f1b56f2a366bfbb30d805f5721dbb
Damilola Olaiya <123437493+Hirosus@users.noreply.github.com>
2023-06-25 03:41:35 +0100

    Updated _printf.c

--- _printf.c
+++ _printf.c
@@ -1,11 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-/**
- * _printf - the function prints string literals to stdo.
- * @format: specific type of character to be printed.
- *
- * Return: number of characters printed
- */
+#define BUFFER_SIZE 1024
+
 int _printf(const char *format, ...)
 {
     va_list args;
@@ -27,43 +26,36 @@ int _printf(const char *format, ...)
             i++; /* Skip the '%' */
 
             /* Check the conversion specifier */
-            switch (format[i])
-            {
+            switch (format[i])                                                                       {
                 case 'c':
                     /* Print a character */
                     count += _putchar(va_arg(args, int));
                     break;
-
                 case 's':
-                    /* Print a string */
-                    count += _puts(va_arg(args, char *));
+                    /* Print a string */                                                                     count += _puts(va_arg(args, char *));
                     break;
-
                 case '%':
                     /* Print a literal '%' */
                     count += _putchar('%');
                     break;
-
                 case 'd':
                 case 'i':
                     /* Print a decimal integer */
                     {
-                        int num = va_arg(args, int);
-                        int length = snprintf(buffer, BUFFER_SIZE, "%d", num);
+                        int num = va_arg(args, int);                                                             int length = snprintf(buffer, BUFFER_SIZE, "%d", num);
                         count += _puts(buffer);
                         count += length;
                     }
                     break;
-
                 default:
-                    /* Unknown conversion specifier, print it */
+                    /* Invalid conversion specifier, ignore */
                     count += _putchar('%');
                     count += _putchar(format[i]);
                     break;
-            }
+}
         }
     }
-    va_end(args);
+
+    va_end(args);                                                                        
     return count;
 }
-
