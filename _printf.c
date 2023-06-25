8683f6f4a46ee375537be6410c658605c449d249
Argen-tal <carlpeacebringer@gmail.com>
2023-06-25 03:48:10 -0700

    implemented error handling for format

--- _printf.c
+++ _printf.c
@@ -3,72 +3,68 @@
 /**
  * _printf - the function prints string literals to stdo.
  * @format: specific type of character to be printed.
- *
+ * % -  Regular character, print it directly, and Print a literal '%'
+ * case c - Print a character.
+ * case s - Print a string.
+ * case d and i - print a decimal integer.
+ * default - Unknown conversion specifier, print it.
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0; /* Number of characters printed */
-    char buffer[BUFFER_SIZE]; /* Local buffer for efficient printing */
-    int i;
+	va_list args;
+	int count = 0; /* Number of characters printed */
+	char buffer[BUFFER_SIZE]; /* Local buffer for efficient printing */
+	int i;
 
-    va_start(args, format);
+	if (format == NULL)
+                return (-1);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            /* Regular character, print it directly */
-            count += _putchar(format[i]);
-        }
-        else
-        {
-            i++; /* Skip the '%' */
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+		}
+		else
+		{
+			i++; /* Skip the '%' */
 
-            /* Check the conversion specifier */
-            switch (format[i])
-            {
-                case 'c':
-                    /* Print a character */
-                    count += _putchar(va_arg(args, int));
-                    break;
-
-                case 's':
-                    /* Print a string */
-                    {
-                        char *str = va_arg(args, char *);
-                        if (str == NULL)
-                            str = "(null)"; /* Handle NULL strings */
-                        count += _puts(str);
-                    }
-                    break;
-
-                case '%':
-                    /* Print a literal '%' */
-                    count += _putchar('%');
-                    break;
-
-                case 'd':
-                case 'i':
-                    /* Print a decimal integer */
-                    {
-                        int num = va_arg(args, int);
-                        int length = snprintf(buffer, BUFFER_SIZE, "%d", num);
-                        count += _puts(buffer);
-                        count += length;
-                    }
-                    break;
-
-                default:
-                    /* Unknown conversion specifier, print it */
-                    count += _putchar('%');
-                    count += _putchar(format[i]);
-                    break;
-            }
-        }
-    }
-    va_end(args);
-    return count;
+			/* Check the conversion specifier */
+			switch (format[i])
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					{
+						char *str = va_arg(args, char *);
+						if (str == NULL)
+							str = "(null)"; /* Handle NULL strings */
+						count += _puts(str);
+					}
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 'd':
+				case 'i':
+					{
+						int num = va_arg(args, int);
+						int length = snprintf(buffer, BUFFER_SIZE, "%d", num);
+						count += _puts(buffer);
+						count += length;
+					}
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i]);
+					break;
+			}
+		}
+	}
+	va_end(args);
+	return (count);
 }
 
