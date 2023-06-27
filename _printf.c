95eeea25dcef54bac7b0f11dba7d2fd9b2460e49
Damilola Olaiya <123437493+Hirosus@users.noreply.github.com>
2023-06-28 00:19:12 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -26,6 +26,10 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 				return (-1);
+			if (format[i] == 'd' || format[i] == 'i')
+			{
+			count += handle_integers(&format[i], args);
+			}
 
 			count = handle_format(format[i], args, &count);
 		}
